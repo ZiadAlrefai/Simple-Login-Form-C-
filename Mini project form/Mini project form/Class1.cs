d0b9637bc51c0dccd295385e1c4ea23894e4dcc2
ZiadAlrefai <106214544+ZiadAlrefai@users.noreply.github.com>
2022-05-25 05:42:42 +0300

    Add files via upload

---
+++ Mini project form/Mini project form/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mini_project_form
+{
+    class Class1
+    {
+    }
+}
---
+++ Mini project form/Mini project form/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini_project_form
+{
+    public partial class Form1 : Form
+    {
+       
+        private Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+     private void button1_Click(object sender, EventArgs e)
+        {
+            string Id = "admin";
+            string Pin = "1234";
+
+            if(textBox1.Text.Equals(Id) && textBox2.Text.Equals(Pin))
+            {
+                MessageBox.Show("Welcome To ATM");
+            }
+            else
+            {
+                MessageBox.Show("Wrong User or Password");
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
